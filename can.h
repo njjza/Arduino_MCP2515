5a188ee56a39f06269b2be90957ffbad95485bcc
njjza <njjza@hotmail.com>
2021-01-02 00:07:35 -0800

    Finish can prototype

--- can.h
+++ can.h
@@ -1,17 +1,31 @@
 //NOTE This file needs to be redesigned after I got my design pattern book back.
-#include "2515.h"
 #include <stdint.h>
+#include <stdbool.h>
 
-#define CAN_STATUS uint8_t
+#define id_stand_mask   2047
+#define id_extend_mask  262143
+
+enum msg_type {
+    STANDARD, EXTENDED, REMOTE, ERROR
+};
 
 struct can_type {
-
+    unsigned int id;
+    bool extend;
 };
 
 /* CAN function prototype */
-void can_init();
-int can_send();
-int can_rx();
-int can_speed();
-int can_spi_tx();
-int can_spi_rx();
+void can_init(int id);
+void can_reset();
+
+bool can_tx();
+bool can_rx();
+int can_rx_mode();
+bool can_tx_cancel();
+bool can_spi_tx();
+bool can_spi_rx();
+
+int can_msg_construct();
+int can_msg_extract();
+
+void can_id_concat(int high, int low);
